98a83b22118ddd8b46b3d320dd9c804ec03c1cde
Benjamin Kim <benkim96@gmail.com>
2020-09-01 12:27:46 -0700

    set up app pages home profile about contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'app/home'
+  get 'app/profile'
+  get 'app/about'
+  get 'app/contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
