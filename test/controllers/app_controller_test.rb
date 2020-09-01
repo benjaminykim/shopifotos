37a78e30396fecfc4fdddb46867ad6f6d4f1a092
Benjamin Kim <benkim96@gmail.com>
2020-09-01 12:52:58 -0700

    set up tests for basic routing

--- test/controllers/app_controller_test.rb
+++ test/controllers/app_controller_test.rb
@@ -1,24 +1,33 @@
 require 'test_helper'
 
 class AppControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "Shopifotos"
+  end
+
   test "should get home" do
-    get app_home_url
+    get home_url
     assert_response :success
+    assert_select "title", "#{@base_title} | Home"
   end
 
   test "should get profile" do
-    get app_profile_url
+    get profile_url
     assert_response :success
+    assert_select "title", "#{@base_title} | Profile"
   end
 
   test "should get about" do
-    get app_about_url
+    get about_url
     assert_response :success
+    assert_select "title", "#{@base_title} | About"
   end
 
   test "should get contact" do
-    get app_contact_url
+    get contact_url
     assert_response :success
+    assert_select "title", "#{@base_title} | Contact"
   end
 
 end
