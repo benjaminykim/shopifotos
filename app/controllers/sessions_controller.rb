83d85ddb1d6568eefd20cd61e439aaf2fd9e1051
Benjamin Kim <benkim96@gmail.com>
2020-09-06 12:34:13 -0700

    header dependent on session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,20 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    email = params[:session][:email].downcase
+    password = params[:session][:password]
+    user = User.find_by(email: email)
+    if (user && user.authenticate(password))
+      log_in user
+      redirect_to user
+    else
+      flash.now[:danger] = "Invalid login credentials"
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
 end
