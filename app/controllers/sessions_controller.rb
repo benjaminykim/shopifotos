d4a5c0f6bb155606047e783b88b72e024b12e560
Benjamin Kim <benkim96@gmail.com>
2020-09-06 21:40:27 -0700

    configure cookie helper and db

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: email)
     if user&.authenticate(password)
       log_in user
+      remember user
       redirect_to user
     else
       flash.now[:danger] = "Invalid login credentials"
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   before_save { email.downcase! }
   validates(:name,
             presence: true,
@@ -19,8 +20,29 @@ class User < ApplicationRecord
 
 
   # return the hash digest of password
-  def User.digest(string)
+  def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  # returns a random token
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # returns user in db for use in persistent sessions
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # returns true if given token matches digest
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # remove user cookie
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
